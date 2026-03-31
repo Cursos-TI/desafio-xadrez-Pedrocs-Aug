f0d43dff4c5990f2c1fe2ecc58ce2af6cc096c1a
Pedro Augusto <pedrosousaugustooo@gmail.com>
2026-03-31 16:33:15 +0000

    Desafio: nível aventureiro Movimentando o Cavalo, entregue.

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 // Função principal do programa
-int main() {
-    
+int main()
+{
+
     // Variáveis para controlar as movimentações das peças
-    int Bispo = 0, Torre = 0, Rainha = 0;
+    int Bispo = 0, Torre = 0, Rainha = 0, Cavalo = 0;
 
     // Movimentação do Bispo
     printf("Movimentação do Bispo:\n");
@@ -22,13 +23,31 @@ int main() {
         printf("Direita\n");
         Torre++;
     } while (Torre < 5);
-    
+
     // Movimentação da Rainha
     printf("\nMovimentação da Rainha:\n");
     for (Rainha; Rainha < 8; Rainha++)
     {
         printf("Esquerda\n");
     }
+
+    // Movimentação do Cavalo
+    printf("\nMovimentação do Cavalo:\n");
+    // O Cavalo tem um movimento especial em "L", então vamos simular isso com um loop DO-While para mostrar a movimentação única do Cavalo. Ele se move duas casas em uma direção e depois uma casa perpendicularmente.
+    int i = 0; // Variável para controlar o movimento perpendicular do Cavalo
+
+    // O Cavalo tem um movimento especial em "L", então vamos simular isso com um loop DO-While para mostrar a movimentação única do Cavalo. Ele se move duas casas em uma direção e depois uma casa perpendicularmente.
+    for (Cavalo = 0; Cavalo < 1; Cavalo++)
+    {   
+        int i = 0;
+        while (i < 2)
+        {
+            printf("Baixo\n");
+            i++;
+        }
+        printf("Esquerda\n");
+    }
     
+
     return 0;
 }
